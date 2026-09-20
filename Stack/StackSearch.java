a60c6c8feadd1df60ed3cb994032be41551b5cdb
Akash Karmakar <akashkarmakar0901@gmail.com>
2026-09-20 23:50:37 +0530

    Add feature to display the position of the searched element

--- Stack/StackSearch.java
+++ Stack/StackSearch.java
@@ -38,19 +38,34 @@ public class StackSearch {
         }
     }
 
-    // method to search for element
-    boolean search(int s) {
+    // method to search for element and determine the position of the found element
+    // from the top
+    int search(int s) {
         if (top == -1) {
             System.out.println("Underflow");
-            return false;
+            return -1;
         } else {
             for (int i = top; i >= 0; i--) {
                 if (s == arr[i]) {
-                    return true;
+                    return top - i + 1; // returns the position of the element from the top
                 }
             }
         }
-        return false;
+        return -1;
+    }
+
+    // method to display the stack
+    void display() {
+        if (top == -1) {
+            System.out.println("Underflow");
+        } else {
+            System.out.println("Stack elements:");
+
+            // loop runs from top to bottom
+            for (int i = top; i >= 0; i--) {
+                System.out.println(arr[i]);
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -61,16 +76,21 @@ public class StackSearch {
 
         StackSearch ob = new StackSearch(n);
 
+        // push elements into stack
         System.out.println("Enter integers to be pushed:");
         for (int i = 0; i < n; i++) {
             int val = sc.nextInt();
             ob.push(val);
         }
 
+        ob.display();
+
         System.out.println("Enter the integer to be searched for:");
         int s = sc.nextInt();
-        if (ob.search(s)) {
-            System.out.println("Element found");
+        int pos = ob.search(s); // stores the position of the element
+
+        if (pos != -1) {
+            System.out.println("Element found at position " + pos + " from the top");
         } else {
             System.out.println("Element not found");
         }
