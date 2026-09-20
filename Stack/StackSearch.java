d943a195e84ccd00be076181603539e1d2c58e29
Akash Karmakar <akashkarmakar0901@gmail.com>
2026-09-20 23:40:09 +0530

    Add stack search program

---
+++ Stack/StackSearch.java
@@ -0,0 +1,79 @@
+package Stack;
+
+import java.util.*;
+
+public class StackSearch {
+    // instance variables
+    int arr[];
+    int size;
+    int top;
+
+    // default constructor to initialize variables
+    StackSearch() {
+        size = 50;
+        top = -1;
+        arr = new int[size];
+    }
+
+    // parameterized constructor to determine the capacity of the stack
+    StackSearch(int cap) {
+        if (cap > 0 && cap <= 50) {
+            size = cap;
+        } else {
+            size = 50;
+            System.out.println("Invalid size. Default size 50 selected");
+        }
+
+        arr = new int[size];
+        top = -1;
+    }
+
+    // method to push elements into the stack
+    void push(int n) {
+        if (top == size - 1) {
+            System.out.println("Overflow");
+        } else {
+            top++;
+            arr[top] = n;
+        }
+    }
+
+    // method to search for element
+    boolean search(int s) {
+        if (top == -1) {
+            System.out.println("Underflow");
+            return false;
+        } else {
+            for (int i = top; i >= 0; i--) {
+                if (s == arr[i]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of the stack:");
+        int n = sc.nextInt();
+
+        StackSearch ob = new StackSearch(n);
+
+        System.out.println("Enter integers to be pushed:");
+        for (int i = 0; i < n; i++) {
+            int val = sc.nextInt();
+            ob.push(val);
+        }
+
+        System.out.println("Enter the integer to be searched for:");
+        int s = sc.nextInt();
+        if (ob.search(s)) {
+            System.out.println("Element found");
+        } else {
+            System.out.println("Element not found");
+        }
+        sc.close();
+    }
+}
