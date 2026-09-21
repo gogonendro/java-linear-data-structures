3a0d12bf86d468d61e83bf3892d92977f8d42679
Akash Karmakar <akashkarmakar0901@gmail.com>
2026-09-21 21:48:35 +0530

    Add comments

--- Stack/StackMin.java
+++ Stack/StackMin.java
@@ -3,16 +3,19 @@ package Stack;
 import java.util.*;
 
 public class StackMin {
+    // instance vars
     int arr[];
     int top;
     int size;
 
+    // default constructor to initialize vars
     StackMin() {
-        size = 50;
+        size = 50; // default size of stack
         top = -1;
         arr = new int[size];
     }
 
+    // parameterized constructor to determine the capacity of stack
     StackMin(int cap) {
         if (cap > 0 && cap <= 50) {
             size = cap;
@@ -24,6 +27,7 @@ public class StackMin {
         top = -1;
     }
 
+    // pushing elements into stack
     void push(int n) {
         if (top == size - 1) {
             System.out.println("Overflow");
@@ -34,6 +38,7 @@ public class StackMin {
         }
     }
 
+    // getting the smallest element in the stack
     void getMin() {
         if (top == -1) {
             System.out.println("Stack empty");
