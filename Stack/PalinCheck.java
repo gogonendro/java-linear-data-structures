d7eed71d64050244beb5a0a380b0727e498642dc
Akash Karmakar <akashkarmakar0901@gmail.com>
2026-09-21 21:41:31 +0530

    Add comments

--- Stack/PalinCheck.java
+++ Stack/PalinCheck.java
@@ -3,20 +3,23 @@ package Stack;
 import java.util.*;
 
 public class PalinCheck {
+    // instance vars
     Scanner sc = new Scanner(System.in);
     String w, rw = "";
     char arr[];
     int l, top;
 
+    // accepting string from user
     void acc() {
         System.out.println("Enter a word:");
         w = sc.next();
         l = w.length();
 
-        arr = new char[l];
+        arr = new char[l]; // creating an array of the length of the string
         top = -1; // top must always be initialized with -1
     }
 
+    // pushing the characters of the string into the stack
     void push() {
         for (int i = 0; i < l; i++) {
             top++; // increase top by 1
@@ -24,6 +27,7 @@ public class PalinCheck {
         }
     }
 
+    // reversing the stack to check for palindrome
     String rev() {
         System.out.println("Reverse of word:");
 
